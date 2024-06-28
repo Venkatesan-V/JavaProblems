3187eeebdd0051f1f4a8ee498bcf075b0b5cead9
Venkatesan-V <venkat.evershine@gmail.com>
2024-06-28 18:00:32 +0530

    Second commit

---
+++ javaproblems/src/main/java/z10javaProb/SwapTwonumbers.java
@@ -0,0 +1,42 @@
+package z10javaProb;
+
+public class SwapTwonumbers {
+
+	public void usingThirdVar() {
+		int mySal=20;
+		int supSal=50;
+		System.out.println("Before swap "+ "Mine :"+mySal +" Supervisor :"+supSal);
+		int temp=mySal;
+		mySal=supSal;
+		supSal=temp;
+		System.out.println("After swap "+ "Mine :"+mySal +" Supervisor :"+supSal);
+	}
+	
+	public void usingMath() {
+		int first=40;
+		int second=90;
+		System.out.println("Before swap "+ "Mine :"+first +" Supervisor :"+second);
+		first=first-second;
+		second=first+second;
+		first=second-first;
+		System.out.println("After swap "+ "Mine :"+first +" Supervisor :"+second);
+	}
+	
+	public void usingMultiplication() {
+		int first=15; //No value should be zero
+		int second=45;
+		System.out.println("Before swap "+ "Mine :"+first +" Supervisor :"+second);
+		first=first*second;
+		second=first/second;
+		first=first/second;
+		System.out.println("After swap "+ "Mine :"+first +" Supervisor :"+second);
+	}
+	public static void main(String[] args) {
+		
+		SwapTwonumbers calling=new SwapTwonumbers();
+		calling.usingThirdVar();
+		calling.usingMath();
+		calling.usingMultiplication();
+		
+	}
+}
